9e265531b91bec954883d743ee238aafb5a34857
Ebad <muhammad.ebad@joybox.pk>
2024-04-18 02:08:06 +0500

    JoyBox Choice Screen Updated

--- lib/common_widgets/common_elevated_button.dart
+++ lib/common_widgets/common_elevated_button.dart
@@ -1,63 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
 import '../res/color.dart';
-
-// class CommonElevatedButton extends StatelessWidget {
-//   const CommonElevatedButton({
-//     super.key,
-//     this.decoration,
-//     this.leftIcon,
-//     this.rightIcon,
-//     this.margin,
-//     required this.onPressed,
-//     this.buttonStyle,
-//     this.alignment,
-//     this.buttonTextStyle,
-//     this.isDisabled,
-//     this.height,
-//     this.width,
-//     this.color,
-//     this.textColor,
-//     required this.text,
-//   });
-
-//   final BoxDecoration? decoration;
-//   final Widget? leftIcon;
-//   final Widget? rightIcon;
-//   final EdgeInsets? margin;
-//   final VoidCallback onPressed;
-//   final ButtonStyle? buttonStyle;
-//   final Alignment? alignment;
-//   final TextStyle? buttonTextStyle;
-//   final bool? isDisabled;
-//   final double? height;
-//   final double? width;
-//   final String text;
-//   final Color? color;
-//   final Color? textColor;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return ElevatedButton(
-//       style: ElevatedButton.styleFrom(
-//         padding: EdgeInsets.symmetric(horizontal: width ?? 8.w, vertical: height ?? 8.h),
-//         shape: RoundedRectangleBorder(
-//           borderRadius: BorderRadius.circular(8.0),
-//         ),
-//         textStyle: buttonTextStyle ??
-//             Theme.of(context).textTheme.displaySmall!.copyWith(
-//                   fontSize: 16.sp,
-//                   fontWeight: FontWeight.w600,
-//                 ),
-//         backgroundColor: color ?? AppColor.red1,
-//         foregroundColor: textColor ?? AppColor.white,
-//       ),
-//       onPressed: onPressed,
-//       child: Text(text),
-//     );
-//   }
-// }
 
 class CommonElevatedButton extends StatelessWidget {
   const CommonElevatedButton(
@@ -68,7 +11,7 @@ class CommonElevatedButton extends StatelessWidget {
       required this.text,
       this.textColor,
       this.buttonColor,
-      this.fontsize});
+      this.fontSize});
 
   final VoidCallback onPressed;
   final double? height;
@@ -76,7 +19,7 @@ class CommonElevatedButton extends StatelessWidget {
   final String text;
   final Color? buttonColor;
   final Color? textColor;
-  final double? fontsize;
+  final double? fontSize;
 
   @override
   Widget build(BuildContext context) {
@@ -93,7 +36,7 @@ class CommonElevatedButton extends StatelessWidget {
         child: Text(
           text,
           style: Theme.of(context).textTheme.bodySmall!.copyWith(
-              fontSize: fontsize ?? 15.sp,
+              fontSize: fontSize ?? 15.sp,
               color: textColor ?? AppColor.white,
               fontWeight: FontWeight.w500),
         ),
--- lib/view/joybox_choice/joybox_choice_screen.dart
+++ lib/view/joybox_choice/joybox_choice_screen.dart
@@ -1,17 +1,14 @@
 import 'package:carousel_slider/carousel_slider.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:joy_box_app/common_widgets/common_appbar.dart';
 import 'package:joy_box_app/common_widgets/common_elevated_button.dart';
 import 'package:joy_box_app/model/joybox_choice_widget_one_model.dart';
+import 'package:joy_box_app/model/joybox_choice_widget_two_model.dart';
 import 'package:joy_box_app/res/color.dart';
 import 'package:joy_box_app/view/joybox_choice/widgets/joybox_choice_widget_one.dart';
 import 'package:joy_box_app/view/joybox_choice/widgets/joybox_choice_widget_two.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
-
-import '../../model/joybox_choice_widget_two_model.dart';
 
 class JoyBoxChoiceScreen extends StatefulWidget {
   const JoyBoxChoiceScreen({super.key});
@@ -23,9 +20,9 @@ class JoyBoxChoiceScreen extends StatefulWidget {
 }
 
 class _JoyBoxChoiceScreenState extends State<JoyBoxChoiceScreen> {
-  int activeIndex1 = 0;
+  int _activeIndex1 = 0;
   final _carouselController1 = CarouselController();
-  int activeIndex2 = 0;
+  int _activeIndex2 = 0;
   final _carouselController2 = CarouselController();
 
   @override
@@ -37,168 +34,188 @@ class _JoyBoxChoiceScreenState extends State<JoyBoxChoiceScreen> {
       ),
       body: SingleChildScrollView(
         child: Container(
-          margin: EdgeInsets.only(bottom: 15.h),
+          margin: EdgeInsets.only(bottom: 40.h),
           child: Column(
             children: [
-              Container(
-                alignment: Alignment.center,
-                height: 340.h,
-                width: double.infinity,
-                color: AppColor.amber,
-                child: Column(
+              _buildFirstCarousel(),
+              SizedBox(height: 30.h),
+              _buildProductStack(),
+              SizedBox(height: 80.h),
+              _buildSecondCarousel(),
+              SizedBox(height: 35.h),
+              _buildIndicator(_activeIndex2,
+                  JoyBoxChoiceWidgetTwoModel.joyBoxChoiceItemsList.length),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildFirstCarousel() {
+    return Container(
+      alignment: Alignment.center,
+      height: 320.h,
+      width: double.infinity,
+      color: AppColor.amber,
+      child: Column(
+        children: [
+          CarouselSlider.builder(
+            itemCount: JoyBoxChoiceWidgetOneModel.joyBoxChoiceItemsList.length,
+            options: CarouselOptions(
+              clipBehavior: Clip.none,
+              viewportFraction: 0.9995,
+              height: 210.h,
+              autoPlay: false,
+              enlargeCenterPage: true,
+              enableInfiniteScroll: true,
+              onPageChanged: (index, reason) {
+                setState(() {
+                  _activeIndex1 = index;
+                });
+              },
+            ),
+            itemBuilder: (context, index, realIndex) {
+              final item =
+                  JoyBoxChoiceWidgetOneModel.joyBoxChoiceItemsList[index];
+              return JoyBoxChoiceWidgetOne(item: item);
+            },
+            carouselController: _carouselController1,
+          ),
+          SizedBox(height: 70.h),
+          _buildIndicator(
+            _activeIndex1,
+            JoyBoxChoiceWidgetOneModel.joyBoxChoiceItemsList.length,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildProductStack() {
+    return SizedBox(
+      height: 600.h,
+      child: Stack(
+        alignment: Alignment.bottomCenter,
+        clipBehavior: Clip.none,
+        children: [
+          Align(
+              alignment: Alignment.topCenter,
+              child: Image.asset("assets/images/joybox_choice_screen_img6.png")),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 30.w),
+            width: double.infinity,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Stack(
                   children: [
-                    CarouselSlider.builder(
-                      itemCount: JoyBoxChoiceWidgetOneModel
-                          .joyBoxChoiceItemsList.length,
-                      options: CarouselOptions(
-                        clipBehavior: Clip.none,
-                        viewportFraction: 0.9995,
-                        height: 210.h,
-                        autoPlay: false,
-                        enlargeCenterPage: true,
-                        enableInfiniteScroll: true,
-                        onPageChanged: (index, reason) {
-                          setState(() {
-                            activeIndex1 = index;
-                          });
-                        },
-                      ),
-                      itemBuilder: (context, index, realIndex) {
-                        final item = JoyBoxChoiceWidgetOneModel
-                            .joyBoxChoiceItemsList[index];
-                        return JoyBoxChoiceWidgetOne(
-                          item: item,
-                        );
-                      },
-                      carouselController: _carouselController1,
+                    Image.asset(
+                      "assets/images/joybox_choice_screen_img2.png",
+                      width: 332.w,
+                      height: 332.h,
+                      fit: BoxFit.cover,
                     ),
-                    SizedBox(height: 85.h),
-                    AnimatedSmoothIndicator(
-                      activeIndex: activeIndex1,
-                      count: JoyBoxChoiceWidgetOneModel
-                          .joyBoxChoiceItemsList.length,
-                      effect: WormEffect(
-                        dotHeight: 12.h,
-                        dotWidth: 12.w,
-                        activeDotColor: AppColor.red2,
+                    Positioned.fill(
+                      bottom: 40.h,
+                      child: Center(
+                        child: CommonElevatedButton(
+                          onPressed: () {},
+                          text: "Add to Cart",
+                          height: 40.h,
+                          width: 95.w,
+                        ),
                       ),
                     ),
                   ],
                 ),
-              ),
-              SizedBox(height: 50.h),
-              Stack(
-                clipBehavior: Clip.none,
-                children: [
-                  Image.asset("assets/images/joybox_choice_screen_img6.png"),
-                  Container(
-                    margin: EdgeInsets.symmetric(horizontal: 30.w),
-                    width: double.infinity,
-                    height: 580.h,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Stack(
-                          children: [
-                            Image.asset(
-                              "assets/images/joybox_choice_screen_img2.png",
-                              width: 332.w,
-                              height: 332.h,
-                              fit: BoxFit.cover,
-                            ),
-                            Positioned.fill(
-                              bottom: 40.h,
-                              child: Center(
-                                child: CommonElevatedButton(
-                                  onPressed: () {},
-                                  text: "Add to Cart",
-                                  height: 6.h,
-                                  width: 12.w,
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                        Stack(
-                          children: [
-                            Image.asset(
-                              "assets/images/joybox_choice_screen_img3.png",
-                              fit: BoxFit.cover,
-                              width: 332.w,
-                              height: 200.h,
-                            ),
-                            Positioned.fill(
-                              left: 8.w,
-                              bottom: -150.h,
-                              child: Center(
-                                child: CommonElevatedButton(
-                                  buttonColor: AppColor.amber2,
-                                  onPressed: () {},
-                                  text: "Add to Cart",
-                                  height: 10.h,
-                                  width: 20.w,
-                                  textColor: AppColor.black,
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
+                SizedBox(height: 50.h),
+                Stack(
+                  children: [
+                    Image.asset(
+                      "assets/images/joybox_choice_screen_img3.png",
+                      fit: BoxFit.cover,
+                      width: 332.w,
+                      height: 200.h,
                     ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 80.h),
-              Stack(
-                children: [
-                  Image.asset(
-                    "assets/images/joybox_choice_screen_img7.png",
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(left: 25.w),
-                    child: CarouselSlider.builder(
-                      itemCount: JoyBoxChoiceWidgetTwoModel
-                          .joyBoxChoiceItemsList.length,
-                      options: CarouselOptions(
-                        padEnds: false,
-                        clipBehavior: Clip.none,
-                        viewportFraction: 0.8,
-                        // Adjust this value to control spacing between items
-                        height: 400.h,
-                        autoPlay: false,
-                        enlargeCenterPage: true,
-                        enableInfiniteScroll: true,
-                        onPageChanged: (index, reason) {
-                          setState(() {
-                            activeIndex2 = index;
-                          });
-                        },
+                    Positioned.fill(
+                      left: 8.w,
+                      bottom: -150.h,
+                      child: Center(
+                        child: CommonElevatedButton(
+                          buttonColor: AppColor.amber2,
+                          onPressed: () {},
+                          text: "Add to Cart",
+                          height: 36.h,
+                          width: 105.w,
+                          textColor: AppColor.black,
+                        ),
                       ),
-                      itemBuilder: (context, index, realIndex) {
-                        final item = JoyBoxChoiceWidgetTwoModel
-                            .joyBoxChoiceItemsList[index];
-                        return JoyBoxChoiceWidgetTwo(
-                          item: item,
-                        );
-                      },
-                      carouselController: _carouselController2,
                     ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 50.h),
-              AnimatedSmoothIndicator(
-                activeIndex: activeIndex2,
-                count: JoyBoxChoiceWidgetTwoModel.joyBoxChoiceItemsList.length,
-                effect: WormEffect(
-                  dotHeight: 12.h,
-                  dotWidth: 12.w,
-                  activeDotColor: AppColor.red2,
+                  ],
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
+        ],
+      ),
+    );
+  }
+
+
+  Widget _buildSecondCarousel() {
+    return SizedBox(
+      height: 420.h,
+      child: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          Align(
+            alignment: Alignment.topCenter,
+            child: Image.asset(
+              "assets/images/joybox_choice_screen_img7.png",
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 25.w),
+            child: CarouselSlider.builder(
+              itemCount:
+                  JoyBoxChoiceWidgetTwoModel.joyBoxChoiceItemsList.length,
+              options: CarouselOptions(
+                padEnds: false,
+                clipBehavior: Clip.none,
+                viewportFraction: 0.8,
+                // Adjust this value to control spacing between items
+                height: 400.h,
+                autoPlay: false,
+                enlargeCenterPage: true,
+                enableInfiniteScroll: true,
+                onPageChanged: (index, reason) {
+                  setState(() {
+                    _activeIndex2 = index;
+                  });
+                },
+              ),
+              itemBuilder: (context, index, realIndex) {
+                final item =
+                    JoyBoxChoiceWidgetTwoModel.joyBoxChoiceItemsList[index];
+                return JoyBoxChoiceWidgetTwo(item: item);
+              },
+              carouselController: _carouselController2,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildIndicator(int activeIndex, int count) {
+    return AnimatedSmoothIndicator(
+      activeIndex: activeIndex,
+      count: count,
+      effect: WormEffect(
+        dotHeight: 12.h,
+        dotWidth: 12.w,
+        activeDotColor: AppColor.red2,
       ),
     );
   }
