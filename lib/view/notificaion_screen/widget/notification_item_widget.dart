66769530a6ca1069edcd5f0d0be9ac96fcb789ee
hafeezullah99 <hafeez.ullah@joybox.pk>
2024-04-22 16:44:45 -0700

    updated the UI issues of Notificationm Screen

--- lib/view/notificaion_screen/widget/notification_item_widget.dart
+++ lib/view/notificaion_screen/widget/notification_item_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:joy_box_app/res/color.dart';
 import 'package:joy_box_app/view/notificaion_screen/model/notification_model.dart';
 
 class NotificationItem extends StatelessWidget {
@@ -7,10 +8,10 @@ class NotificationItem extends StatelessWidget {
   final Function(NotificationModel) onDismissed;
 
   const NotificationItem({
-    Key? key,
+    super.key,
     required this.notification,
     required this.onDismissed,
-  }) : super(key: key);
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -20,10 +21,51 @@ class NotificationItem extends StatelessWidget {
         onDismissed(notification);
       },
       background: Container(
-        color: Colors.red, // Background color when swiping to dismiss
+        color: Colors.red, // Background color when swiping to dismiss from left
+        alignment: Alignment.centerLeft,
+        padding: EdgeInsets.symmetric(horizontal: 20.w),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              "assets/images/notification_screen_img4.png",
+              color: AppColor.white,
+            ),
+            Text(
+              "Remove",
+              style: Theme.of(context).textTheme.displaySmall!.copyWith(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 12.sp,
+                    color: AppColor.white,
+                  ),
+            )
+          ],
+        ),
+      ),
+      secondaryBackground: Container(
+        color: AppColor
+            .blue, // Background color when swiping to dismiss from right
         alignment: Alignment.centerRight,
         padding: EdgeInsets.symmetric(horizontal: 20.w),
-        child: Icon(Icons.delete, color: Colors.white),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Icon(
+              Icons.archive,
+              color: AppColor.white,
+            ),
+            Text(
+              "Archive",
+              style: Theme.of(context).textTheme.displaySmall!.copyWith(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 12.sp,
+                    color: AppColor.white,
+                  ),
+            )
+          ],
+        ),
       ),
       child: Container(
         color: Colors.amberAccent.shade100,
@@ -34,7 +76,7 @@ class NotificationItem extends StatelessWidget {
             Padding(
               padding: EdgeInsets.symmetric(vertical: 10.h, horizontal: 10.w),
               child: Text(
-                notification.time.toString(),
+                notification.getFormattedTime(),
                 style: Theme.of(context).textTheme.bodySmall!.copyWith(
                       fontWeight: FontWeight.w100,
                       fontSize: 12.sp,
