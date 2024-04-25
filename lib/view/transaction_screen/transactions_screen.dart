b16d48d12ca136ae00ccb2e98ee360f5c02114a8
hafeezullah99 <hafeez.ullah@joybox.pk>
2024-04-25 16:58:59 -0700

    updated Transaction Screen

--- lib/view/transaction_screen/transactions_screen.dart
+++ lib/view/transaction_screen/transactions_screen.dart
@@ -1,9 +1,15 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:joy_box_app/common_widgets/common_appbar.dart';
 import 'package:joy_box_app/common_widgets/custom_image_view.dart';
 import 'package:joy_box_app/res/color.dart';
+import 'package:joy_box_app/view/transaction_screen/widgets/transaction_balance_widget.dart';
+import 'package:joy_box_app/view/transaction_screen/widgets/transaction_inner_widget.dart';
+
+import 'model/transactions_inner_widget_model.dart';
 
 class TransactionsScreen extends StatefulWidget {
   const TransactionsScreen({super.key});
@@ -28,52 +34,60 @@ class _TransactionsScreenState extends State<TransactionsScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const CommonAppBar(
+        isCircular: true,
         text: "Transaction",
       ),
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            BuildTabs(context),
+            buildTabs(context),
             const TransactionBalanceWidget(),
-            CustomImageView(
-              fit: BoxFit.cover,
-              imagePath: "assets/images/Group 289413.png",
+            SizedBox(
+              height: 5.h,
             ),
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Transactions",
-                    style: Theme.of(context)
-                        .textTheme
-                        .displaySmall!
-                        .copyWith(fontSize: 16.sp, fontWeight: FontWeight.w600),
-                  ),
-                  SizedBox(
-                    height: 20.h,
-                  ),
-                  Container(
-                    height: 40.h,
-                    width: 400.w,
-                    color: AppColor.black.withOpacity(0.1),
-                    child: ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      itemCount: items.length,
-                      itemBuilder: (context, index) {
-                        final item = items[index];
-                        return buildTransactionTabItem(item, context, index);
-                      },
+            _PaymentWidget(),
+            Material(
+              elevation: 20,
+              borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(15), topRight: Radius.circular(15)),
+              child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 20.h,
                     ),
-                  ),
-                  const TransactionInnerWidet(),
-                  const Divider(),
-                  const TransactionInnerWidet(),
-                  const Divider(),
-                  const TransactionInnerWidet(),
-                ],
+                    Text(
+                      "Transactions",
+                      style: Theme.of(context).textTheme.displaySmall!.copyWith(
+                          fontSize: 16.sp, fontWeight: FontWeight.w600),
+                    ),
+                    SizedBox(
+                      height: 20.h,
+                    ),
+                    Container(
+                      height: 40.h,
+                      width: 370.w,
+                      decoration: BoxDecoration(
+                          color: AppColor.black.withOpacity(0.1),
+                          borderRadius: BorderRadius.circular(5)),
+                      child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: items.length,
+                        itemBuilder: (context, index) {
+                          final item = items[index];
+                          return buildTransactionTabItem(item, context, index);
+                        },
+                      ),
+                    ),
+                    SizedBox(
+                        height: 250.h,
+                        child:
+                            TransactionInnerWidet(transactions: transactions))
+                  ],
+                ),
               ),
             )
           ],
@@ -82,7 +96,7 @@ class _TransactionsScreenState extends State<TransactionsScreen> {
     );
   }
 
-  Widget BuildTabs(BuildContext context) {
+  Widget buildTabs(BuildContext context) {
     return SingleChildScrollView(
       scrollDirection: Axis.horizontal,
       child: Padding(
@@ -109,13 +123,12 @@ class _TransactionsScreenState extends State<TransactionsScreen> {
         setState(() {
           // Handle tab selection
           selectedIndex = index;
-          print("Tabbed");
         });
       },
       child: Container(
         padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
         decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(6),
+          borderRadius: BorderRadius.circular(5),
           color: selectedIndex == index ? AppColor.amber : null,
         ),
         child: Text(
@@ -136,11 +149,10 @@ class _TransactionsScreenState extends State<TransactionsScreen> {
       onTap: () {
         setState(() {
           selectedTransactionIndex = index;
-          print("Tabbed");
         });
       },
       child: Container(
-        width: 120.w,
+        width: 118.w,
         decoration: BoxDecoration(
           color: isSelected ? Colors.red : null,
           borderRadius: BorderRadius.circular(6),
@@ -160,144 +172,108 @@ class _TransactionsScreenState extends State<TransactionsScreen> {
   }
 }
 
-class TransactionBalanceWidget extends StatelessWidget {
-  const TransactionBalanceWidget({
-    super.key,
-  });
+class _PaymentWidget extends StatelessWidget {
+  _PaymentWidget({super.key});
+
+  final List cardimages = [
+    "assets/images/transaction_img6.png",
+    "assets/images/transaction_img5.png"
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Container(
-          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            boxShadow: const [
-              BoxShadow(
-                  color: Colors.black12,
-                  blurRadius: 2,
-                  blurStyle: BlurStyle.outer,
-                  offset: Offset(2, 1),
-                  spreadRadius: 2)
-            ],
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "Your balance",
-                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                      // fontSize: 14.sp,
-                      fontWeight: FontWeight.w400,
-                    ),
-              ),
-              RichText(
-                text: TextSpan(
-                  children: [
-                    TextSpan(
-                      text: 'RS .',
-                      style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                            // fontSize: 14.sp,
-                            fontWeight: FontWeight.w400,
-                          ),
-                    ),
-                    TextSpan(
-                      text: ' 130.000',
-                      style:
-                          Theme.of(context).textTheme.displayMedium!.copyWith(
-                                // fontSize: 14.sp,
-                                fontWeight: FontWeight.w400,
-                              ),
-                    ),
-                  ],
-                ),
-              )
-            ],
-          ),
-        ),
-        SizedBox(
-          width: 50.w,
-        ),
-        Column(
-          children: [
-            Text(
-              "18 Jan 2019",
-              style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                    // fontSize: 14.sp,
-                    fontWeight: FontWeight.w400,
-                  ),
-            ),
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 20.h),
-              decoration: BoxDecoration(
-                  border: Border.all(color: Colors.amberAccent),
-                  borderRadius: BorderRadius.circular(6.h)),
-              child: const Icon(
-                Icons.add,
-                color: Colors.red,
-              ),
-            )
-          ],
-        )
-      ],
-    );
-  }
-}
-
-class TransactionInnerWidet extends StatelessWidget {
-  const TransactionInnerWidet({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 20),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return Container(
+      width: double.infinity,
+      color: AppColor.amber.withOpacity(0.1),
+      child: Stack(
         children: [
-          Row(
-            children: [
-              CustomImageView(
-                imagePath: "assets/images/transactions_screen_img3.jpg",
-              ),
-              SizedBox(
-                width: 10.w,
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+          CustomImageView(
+            imagePath: "assets/images/transaction_img4.png",
+            color: AppColor.amber,
+          ),
+          Positioned(
+            top: 30.h,
+            left: 0,
+            right: 0,
+            child: Container(
+              width: double.infinity,
+              child: Column(
                 children: [
-                  Text(
-                    "Burger O’Clock",
-                    style: Theme.of(context).textTheme.displaySmall!.copyWith(
-                          fontWeight: FontWeight.w600,
-                          fontSize: 16.sp,
-                        ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Text("Payment method",
+                          style: Theme.of(context)
+                              .textTheme
+                              .displaySmall!
+                              .copyWith(
+                                fontWeight: FontWeight.w600,
+                                fontSize: 14.sp,
+                              )),
+                      InkWell(
+                        onTap: () {},
+                        child: Text("Add Cash",
+                            style: Theme.of(context)
+                                .textTheme
+                                .displaySmall!
+                                .copyWith(
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 14.sp,
+                                )),
+                      )
+                    ],
                   ),
-                  Text("Food n Drinks",
-                      style: Theme.of(context).textTheme.displaySmall!.copyWith(
-                            fontWeight: FontWeight.w400,
-                            fontSize: 12.sp,
-                          ))
+                  SizedBox(
+                    height: 20.h,
+                  ),
+                  Row(
+                    children: [
+                      SizedBox(
+                        width: 20.w,
+                      ),
+                      Container(
+                        height: 170,
+                        width: 50.w,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          border: Border.all(color: AppColor.amber),
+                          color: AppColor.white.withOpacity(0.7),
+                        ),
+                        child: Center(
+                          child: Icon(
+                            Icons.add,
+                            size: 30.sp,
+                            color: AppColor.red1,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 3.w,
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(6),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(6),
+                            color: AppColor.white.withOpacity(0.8)),
+                        height:
+                            170, // Adjust this height according to your layout requirements
+                        width: MediaQuery.of(context).size.width -
+                            80.w, // Adjust width based on available space
+                        child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: cardimages.length,
+                          itemBuilder: (context, index) {
+                            return CustomImageView(
+                              imagePath: cardimages[index],
+                            );
+                          },
+                        ),
+                      )
+                    ],
+                  ),
                 ],
               ),
-            ],
-          ),
-          Column(
-            children: [
-              Text("Rs. 3000",
-                  style: Theme.of(context).textTheme.displaySmall!.copyWith(
-                      fontWeight: FontWeight.w600,
-                      fontSize: 16.sp,
-                      color: Colors.red)),
-              Text("24th Nov 2023",
-                  style: Theme.of(context).textTheme.displaySmall!.copyWith(
-                        fontWeight: FontWeight.w400,
-                        fontSize: 12.sp,
-                      ))
-            ],
+            ),
           )
         ],
       ),
