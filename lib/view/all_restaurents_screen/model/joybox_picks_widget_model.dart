ea290cf5c518af49f44b64babf5b4c7469770b96
Ebad <muhammad.ebad@joybox.pk>
2024-04-27 04:56:00 +0500

    All Restaurants Updated 4/27/2024

--- lib/view/all_restaurents_screen/model/joybox_picks_widget_model.dart
+++ lib/view/all_restaurents_screen/model/joybox_picks_widget_model.dart
@@ -1,13 +1,40 @@
-class JoyboxPicksWidgetModel {
+class JoyBoxPicksWidgetModel {
   final String name;
   final String imagePath;
   final String description;
   final int rating;
 
-  JoyboxPicksWidgetModel({
+  JoyBoxPicksWidgetModel({
     required this.description,
     required this.rating,
     required this.name,
     required this.imagePath,
   });
+
+  static final List<JoyBoxPicksWidgetModel> joyBoxPicksList = [
+    JoyBoxPicksWidgetModel(
+      rating: 4,
+      name: "Lachine",
+      imagePath: "assets/images/Joybox_picks_widget_img1.png",
+      description: 'Lebanese restaurant & an Italian eatery',
+    ),
+    JoyBoxPicksWidgetModel(
+      rating: 4,
+      name: "Movenpick",
+      imagePath: "assets/images/Joybox_picks_widget_img1.png",
+      description: 'Lebanese restaurant & an Italian eatery',
+    ),
+    JoyBoxPicksWidgetModel(
+      rating: 3,
+      name: "Bam-Bou",
+      imagePath: "assets/images/Joybox_picks_widget_img1.png",
+      description: 'Lebanese restaurant & an Italian eatery',
+    ),
+    JoyBoxPicksWidgetModel(
+      rating: 3,
+      name: "Movenpick",
+      imagePath: "assets/images/Joybox_picks_widget_img1.png",
+      description: 'Lebanese restaurant & an Italian eatery',
+    ),
+  ];
 }
--- lib/view/all_restaurents_screen/widget/joybox_picks_item_widget.dart
+++ lib/view/all_restaurents_screen/widget/joybox_picks_item_widget.dart
@@ -6,62 +6,63 @@ import '../../../common_widgets/common_elevated_button.dart';
 import '../../../common_widgets/custom_image_view.dart';
 import '../../../common_widgets/custom_rating_bar.dart';
 import '../../../res/color.dart';
-import '../model/tradtional_restaurent_widget_model.dart';
+import '../model/joybox_picks_widget_model.dart';
 
-class TraditionalRestaurantsItemWidget extends StatelessWidget {
-  const TraditionalRestaurantsItemWidget({
+class JoyBoxPicksItemWidget extends StatelessWidget {
+  const JoyBoxPicksItemWidget({
     super.key,
     required this.item,
   });
 
-  final TraditionalRestaurantsWidgetModel item;
+  final JoyBoxPicksWidgetModel item;
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Stack(
           clipBehavior: Clip.none,
           children: [
             Material(
+              elevation: 20,
               borderRadius: BorderRadius.circular(10),
-              elevation: 10,
               child: ClipRRect(
                 borderRadius: BorderRadius.circular(10),
                 child: CustomImageView(
-                  width: 350.w,
-                  height: 200.h,
+                  width: 165.w,
+                  height: 280.h,
+                  fit: BoxFit.cover,
                   imagePath: item.imagePath,
                 ),
               ),
             ),
             Positioned(
-              top: -10.h,
+              top: -12.h,
               child: Container(
-                height: 32.h,
+                height: 30.h,
                 alignment: Alignment.centerLeft,
-                padding: EdgeInsets.symmetric(horizontal: 20.h),
+                padding: EdgeInsets.only(right: 28.w, left: 12.w),
                 decoration: BoxDecoration(
                   color: AppColor.amber,
                   borderRadius: BorderRadius.only(
-                      topRight: Radius.circular(4.0),
-                      bottomRight: Radius.circular(4.0)),
+                      topRight: Radius.circular(5),
+                      bottomRight: Radius.circular(5)),
                 ),
                 child: Text(
                   item.name,
                   style: TextStyle(
-                    fontWeight: FontWeight.w500,
+                    fontWeight: FontWeight.w400,
                     color: Colors.black,
-                    fontSize: 12.sp,
+                    fontSize: 14.sp,
                   ),
                 ),
               ),
             ),
             Positioned(
-              top: 28.h,
-              left: -2.w,
+              top: 30.h,
+              left: 10.w,
               child: Container(
-                margin: const EdgeInsets.symmetric(horizontal: 10),
                 width: 130.w,
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
@@ -74,7 +75,6 @@ class TraditionalRestaurantsItemWidget extends StatelessWidget {
                         fontSize: 12.sp,
                       ),
                     ),
-                    SizedBox(height: 3.h),
                     CommonRatingBar(
                       itemCount: 5,
                       color: AppColor.amber,
@@ -86,8 +86,8 @@ class TraditionalRestaurantsItemWidget extends StatelessWidget {
               ),
             ),
             Positioned(
-              top: -6.h,
-              right: -4.w,
+              top: -7.h,
+              right: -7.w,
               child: IconButton(
                 onPressed: () {},
                 icon: Icon(
@@ -97,22 +97,22 @@ class TraditionalRestaurantsItemWidget extends StatelessWidget {
                 ),
               ),
             ),
-            Positioned.fill(
-              top: 80.h,
-              child: Center(
-                child: CommonElevatedButton(
-                    borderRadius: 10.0,
-                    buttonColor: AppColor.red2.withOpacity(0.6),
-                    fontSize: 12.sp,
-                    height: 32.h,
-                    width: 120.w,
-                    onPressed: () {},
-                    text: "Order now"),
+            Positioned(
+              left: 18.w,
+              bottom: 50.h,
+              child: CommonElevatedButton(
+                borderRadius: 12.0,
+                width: 128.w,
+                height: 35.h,
+                buttonColor: AppColor.red2.withOpacity(0.8),
+                fontSize: 13.sp,
+                onPressed: () {},
+                text: "Order now",
               ),
             ),
             Positioned(
-              bottom: 6.h,
-              right: 10.w,
+              bottom: 8.h,
+              right: 14.w,
               child: Image.asset(
                 "assets/images/popular_restaurants_screen_img2.png",
                 height: 12.h,
@@ -144,14 +144,6 @@ class TraditionalRestaurantsItemWidget extends StatelessWidget {
               ),
             ),
           ],
-        ),
-        SizedBox(height: 6.h),
-        Text(
-          item.openingHours,
-          style: Theme.of(context).textTheme.displaySmall!.copyWith(
-                fontSize: 9.sp,
-                fontWeight: FontWeight.w500,
-              ),
         ),
       ],
     );
